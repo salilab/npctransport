d55a22c2684752bbdff78a1bc7a9fccf4c15d20f
Barak Raveh <barak.raveh@gmail.com>
2012-07-11 17:14:02 -0700

    fixing merge bugs II

--- npctransport/bin/fg_cylinder.cpp
+++ npctransport/bin/fg_cylinder.cpp
@@ -11,24 +11,21 @@
 
 
 int main(int argc, char *argv[]) {
-  IMP_NPC_STARTUP;
+  using namespace IMP;
+  using namespace IMP::npctransport;
+  IMP_NPC_STARTUP(sd);
   RMF:: set_show_hdf5_errors(true);
   //  sd->add_interaction(type_of_float[0], type_of_fg[0]);
   //  sd->add_interaction(type_of_float[1], type_of_fg[0]);
 
   //place fg anchors in grid and don't optimize them
+  algebra::Cylinder3D cyl= sd->get_cylinder();
 
-
-
-  IMP::algebra::Cylinder3D cyl= sd->get_cylinder();
-
-
-  using namespace IMP::npctransport;
   atom::Hierarchy root= sd->get_root();
   atom::Hierarchies chains =get_fg_chains(root);
   // create a set of random sites (for now)
-  Vector3Ds sites=get_grid_surface_cover(cyl, sqrt(chains.size())+1,
-                                         sqrt(chains.size())+1);
+  algebra::Vector3Ds sites=get_grid_surface_cover(cyl, sqrt(chains.size())+1,
+                                                  sqrt(chains.size())+1);
   std::cout << IMP::base::Showable(sites) << std::endl;
   // anchor each fg chain to the (x,y) site (only by x,y coords,
   // all anchored to the same z plane)
@@ -38,6 +35,6 @@ int main(int argc, char *argv[]) {
     d.set_coordinates(sites[i]);
     d.set_coordinates_are_optimized(false);
   }
-  IMP_NPC_LOOP(ParticlePairsTemp());
+  IMP_NPC_LOOP(sd, ParticlePairsTemp());
   return 0;
 }
