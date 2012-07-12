593f8db7ef8c0b0c8e167679dc6f0c825f43d8f9
drussel <drussel@gmail.com>
2012-07-12 13:42:58 -0700

    Catch exceptions to produce error messages on systems where the default main doesn't do this already.

--- npctransport/bin/make_correlation.py
+++ npctransport/bin/make_correlation.py
@@ -20,16 +20,20 @@ fg= IMP.npctransport.add_fg_type(config,
 IMP.npctransport.create_range(fg.rest_length_factor, .5, .8, 3)
 
 kap= IMP.npctransport.add_float_type(config,
-                                  number=1,
-                                  radius=10)
+                                     number=1,
+                                     radius=10)
 IMP.npctransport.create_range(kap.number,0, 10,3)
 IMP.npctransport.create_range(kap.interactions, 1, 10, 3)
 
-interaction= IMP.npctransport.add_interaction(config, "fg0", "fg0",
-                                              interaction_k = 1.0,
-                                              interaction_range = 1.0)
+interaction= IMP.npctransport.add_interaction(config, "fg0", "fg0")
 IMP.npctransport.create_range(interaction.is_on,0,1,2)
 
 interaction= IMP.npctransport.add_interaction(config, "fg0", "kap")
+
+crap= IMP.npctransport.add_float_type(config,
+                                      number=1,
+                                      radius=10)
+IMP.npctransport.create_range(crap.number,0, 10,3)
+
 
 IMP.npctransport.write(config)
