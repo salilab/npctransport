6ba0da631f6a2566db7eb09898656609519f225f
Barak Raveh <barak@salilab.org>
2012-11-21 18:08:08 -0800

    tester for iteration through avro files with Avro2PBReader

---
+++ npctransport/test/test_avro_reader.py
@@ -0,0 +1,22 @@
+from IMP.npctransport import *
+import IMP.test
+import sys
+
+class Tests(IMP.test.TestCase):
+  def test_Avro2PBReader(self):
+    """ Testing whether an avro file is read properly by Avro2PBReader """
+    in_avro= self.get_input_file_name( "avro.sample" );
+    print "parsing", in_avro
+    a=Avro2PBReader(in_avro)
+    o = Output()
+    while(a.get_is_valid()):
+      s = a.read_next()
+      if(not a.get_is_valid()): break;
+      o.ParseFromString(s)
+      rg = o.statistics.fgs[0].radius_of_gyration
+#     print rg
+    self.assertAlmostEqual(rg, 40.2645193868, 7)
+
+
+if __name__ == '__main__':
+    IMP.test.main()
