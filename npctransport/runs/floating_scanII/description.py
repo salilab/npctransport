8c5dd97e01c26b7279904d6f2789d9fd59ba23ba
Barak Raveh <barak.raveh@gmail.com>
2012-11-27 17:32:15 -0800

    floating scan dense

---
+++ npctransport/runs/floating_scanII/description.py
@@ -0,0 +1,8 @@
+binary="fg_simulation"
+extra_arguments=[]
+tasks=[562500]
+iterations=1
+description=["""
+A test for a single floating FG nup in the presence of kaps and competitors
+under varying conditions (number of beads, number of nups, number of kaps, number of craps)
+"""]
