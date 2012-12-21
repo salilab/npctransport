9e65d7514e9b21ec4443b257ba2beb97f13158f3
Barak Raveh <barak.raveh@gmail.com>
2012-12-21 13:50:45 -0800

    adjust number of work units for new runs

--- npctransport/runs/cyl_scan_nbeadsFG_kFG_kKap_Rfloat_Rkap10/description.py
+++ npctransport/runs/cyl_scan_nbeadsFG_kFG_kKap_Rfloat_Rkap10/description.py
@@ -1,6 +1,6 @@
 binary="fg_simulation"
 extra_arguments=["--cylinder_nlayers", "4"]
-tasks=2500
+tasks=980000
 iterations=20
 description=["""
 20x500ns simulations of transport with certain parameters
