dbb2f5f487319efb45182c7c14c00b51d9d0283b
Daniel Russel <drussel@gmail.com>
2012-12-27 15:26:16 -0800

    use less problematic compilation method for avro

--- npctransport/src/avro.cpp
+++ npctransport/src/avro.cpp
@@ -2,17 +2,15 @@
 #include <IMP/npctransport/avro.h>
 #include <avro/ValidSchema.hh>
 #include <avro/Compiler.hh>
+#include <avro/Stream.hh>
 #include <IMP/base/file.h>
 
 IMPNPCTRANSPORT_BEGIN_NAMESPACE
 
 avro::ValidSchema get_avro_data_file_schema() {
   std::string path = get_data_path("AvroDataFileData.json");
-  base::TextInput in(path);
-  std::stringstream buf;
-  buf << in.get_stream().rdbuf();
-  avro::ValidSchema result= avro::compileJsonSchemaFromString(buf.str());
-  return result;
+  std::auto_ptr<avro::InputStream> is= avro::fileInputStream(path.c_str());
+  return avro::compileJsonSchemaFromStream(*is);
 }
 
 IMPNPCTRANSPORT_END_NAMESPACE
