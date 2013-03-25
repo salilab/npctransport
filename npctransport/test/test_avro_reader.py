aa4d51f80d740a6e22a7f5cd938a457d305ab159
Barak Raveh <barak.raveh@gmail.com>
2013-03-25 14:40:18 -0700

    fix to test_avro_reader

--- npctransport/test/test_avro_reader.py
+++ npctransport/test/test_avro_reader.py
@@ -5,10 +5,10 @@ import sys
 class Tests(IMP.test.TestCase):
   def test_Avro2PBReader(self):
     """ Testing whether an avro file is read properly by Avro2PBReader """
-    in_avro= self.get_input_file_name( "avro.sample" );
+    in_avro= self.get_input_file_name( "avro.sample");
     print "parsing", in_avro
-    a=Avro2PBReader(in_avro)
-    o = Output()
+    a=Avro2PBReader([in_avro])
+    o = IMP.npctransport.Output()
     while(a.get_is_valid()):
       s = a.read_next()
       if(not a.get_is_valid()): break;
@@ -16,7 +16,6 @@ class Tests(IMP.test.TestCase):
       rg = o.statistics.fgs[0].radius_of_gyration
 #     print rg
     self.assertAlmostEqual(rg, 40.2645193868, 7)
-
 
 if __name__ == '__main__':
     IMP.test.main()
