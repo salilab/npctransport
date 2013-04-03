1fce8d8defe9858c52d0df8b17d534184fc85f46
Daniel Russel <drussel@gmail.com>
2013-04-02 21:34:44 -0700

    dont depend on the avro in RMF

--- npctransport/dependency/AvroCpp.cmake
+++ npctransport/dependency/AvroCpp.cmake
@@ -1,13 +1,9 @@
 
-if (${RMF_AVRO} MATCHES "internal")
-  message(STATUS "Using internal AvroCPP")
-  set(AVROGENCPP ${RMF_BINARY_DIR}/AvroCpp/avrogencpp CACHE INTERNAL "" FORCE)
-  set(AVROGENCPP_DEPENDENCY ${RMF_BINARY_DIR}/AvroCpp/avrogencpp CACHE INTERNAL "" FORCE)
-  set(AVROCPP_INCLUDE_PATH ${PROJECT_SOURCE_DIR}/modules/rmf/dependency/AvroCpp CACHE INTERNAL "" FORCE)
-  set(ACROCPP_LIBRARIES avrocpp CACHE INTERNAL "" FORCE)
-  file(WRITE "${PROJECT_BINARY_DIR}/data/build_info/AvroCpp" "ok=True
-includepath=\"${AvroCpp_INCLUDE_PATH}\"
+message(STATUS "Using internal AvroCPP")
+set(AVROCPP_BINARY_DIR ${PROJECT_BINARY_DIR}/src/dependency/AvroCpp CACHE INTERNAL "" FORCE)
+add_subdirectory("${PROJECT_SOURCE_DIR}/modules/npctransport/dependency/AvroCpp" "${AVROCPP_BINARY_DIR}")
+set(AVROCPP_INCLUDE_PATH "${PROJECT_SOURCE_DIR}/modules/npctransport/dependency/AvroCpp/" CACHE INTERNAL "" FORCE)
+set(AVROCPP_LIBRARIES avrocpp CACHE INTERNAL "" FORCE)
+file(WRITE "${PROJECT_BINARY_DIR}/data/build_info/AvroCpp" "ok=True
+includepath=\"${AVROCPP_INCLUDE_PATH}\"
 ")
-else()
-  set(AVROGENCPP avrogencpp CACHE INTERNAL "" FORCE)
-endif()
