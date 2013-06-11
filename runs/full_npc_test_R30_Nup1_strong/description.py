6537b7224f1bff9edc942dd2b9fcf900650037ff
Barak Raveh <barak@salilab.org>
2013-06-10 21:24:44 -0700

    NPC tests with strong Nup1 (fg10)

---
+++ runs/full_npc_test_R30_Nup1_strong/description.py
@@ -0,0 +1,10 @@
+binary="fg_simulation"
+extra_arguments=["--restart", "config.pb"]
+tasks=50000
+#tasks=2500
+iterations=15
+description=["""
+15x1000ns (=15 microsec) additional simulation time of transport through semi-real NPC
+based on Nature 2007 model FG anchor points, starting from a simulation
+that's been equilibrated for ~5 microsec
+"""]
