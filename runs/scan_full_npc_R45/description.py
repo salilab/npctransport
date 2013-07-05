822b5594a740372b095060c30521621092820991
Barak Raveh <barak@salilab.org>
2013-07-05 12:08:09 -0700

    run for scanning k between kap and fg, and between fg and fg, for R=45 on full size NPC

---
+++ runs/scan_full_npc_R45/description.py
@@ -0,0 +1,10 @@
+binary="fg_simulation"
+extra_arguments=[]
+tasks=30000
+#tasks=2500
+iterations=10
+description=["""
+10x1000ns (=10 microsec) additional simulation time of transport through semi-real NPC
+based on Nature 2007 model FG anchor points, starting from a simulation
+that's been equilibrated for ~2.5 microsec, for a variety of k params
+"""]
