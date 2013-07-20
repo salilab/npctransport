fb4d799b3609bc16821f7281c36dfa4857bfc10c
Barak Raveh <barak@salilab.org>
2013-07-19 17:46:11 -0700

    fixed nframes bug in scan_full_npc_R40

--- runs/scan_full_npc_R40/edit_out.py
+++ runs/scan_full_npc_R40/edit_out.py
@@ -14,27 +14,39 @@ def is_fg_fg(i):
     """ checks if interaction i is between fg and fg """
     return ( re.match("fg", i.type0) and re.match("fg", i.type1) ) \
 
+def change_simulation_time(a, t_ns):
+    """
+    a - protobuf assignment object
+    t - new time in ns
+    """
+    old_t_ns = a.simulation_time_ns
+    time_step_ps = a.time_step # in picosecond = 10E-6 ns
+    ps_in_ns = 1000000
+    a.simulation_time_ns=t_ns
+    a.number_of_frames = int( math.ceil(t_ns * ps_in_ns / time_step_ps) )
+
+
 # fetch params
 # Usage: <cmd> <outfile> <newoutfle>
 outfile = sys.argv[1]
+newoutfile = sys.argv[2]
+new_diffusers_R = 40 # A
 f=open(outfile, "rb")
 output= Output()
 output.ParseFromString(f.read())
 f.close()
 assignment = output.assignment
-assignment.simulation_time_ns=1000
+change_simulation_time(assignment, 1000)
 for floater in assignment.floaters:
-    floater.radius.value = 40
-# for k1 in arange(2.5,4.51,0.5):
-#     for k2 in arange(2.5,4.51,0.5):
-#         for i in assignment.interactions:
-#             if (is_fg_kap(i)) :
-#                 i.interaction_k.value = k1
-#             if (is_fg_fg(i)) :
-#                 i.interaction_k.value = k2
-#         outfile_kk = "%s_%.1f_%.1f" % (outfile, k1, k2)
-#         TODO: output here
-
-# dump to same file
-nf=open(outfile, "wb")
-nf.write(output.SerializeToString())
+    floater.radius.value = new_diffusers_R
+for k1 in arange(2.5,4.51,0.5):
+    for k2 in arange(2.5,4.51,0.5):
+        for i in assignment.interactions:
+            if (is_fg_kap(i)) :
+                i.interaction_k.value = k1
+            if (is_fg_fg(i)) :
+                i.interaction_k.value = k2
+        # dump to new file
+        newoutfile_kk = "%s_%.1f_%.1f" % (newoutfile, k1, k2)
+        nf=open(newoutfile_kk, "wb")
+        nf.write(output.SerializeToString())
