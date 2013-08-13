3afa813cdb1e8cd4ed5ae4ad641ac52b991eb838
Barak Raveh <barak@salilab.org>
2013-08-13 15:44:57 -0700

    new run

---
+++ runs/NPC2007_coarse_scan_FGKapKfactor_Nup1KapK/description.py
@@ -0,0 +1,14 @@
+binary="fg_simulation"
+short_init_factor=0.1
+conformations=""
+extra_arguments=[]
+tasks=300000
+#tasks=2500
+iterations=10
+description=["""
+10x1000ns (=10 microsec) iterations of transport in coarse grained
+NPC2007 model (FG motifs per bead = 2)
+scanning:
+1) k between kaps-fg
+2) k between kaps-Nup1 specifically
+"""]
