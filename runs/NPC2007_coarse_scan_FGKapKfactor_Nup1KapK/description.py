740330fbc0173b24289d7bdbbd4fe259cd3cdf1e
Barak Raveh <barak@salilab.org>
2013-08-14 14:56:01 -0700

    increase # of iterations

--- runs/NPC2007_coarse_scan_FGKapKfactor_Nup1KapK/description.py
+++ runs/NPC2007_coarse_scan_FGKapKfactor_Nup1KapK/description.py
@@ -3,9 +3,9 @@ extra_arguments=["--short_init_factor=.1"]
 tasks=30
 #300000
 #tasks=2500
-iterations=10
+iterations=20
 description=["""
-10x1000ns (=10 microsec) iterations of transport in coarse grained
+20x1000ns (=10 microsec) iterations of transport in coarse grained
 NPC2007 model (FG motifs per bead = 2)
 scanning:
 1) k between kaps-fg
