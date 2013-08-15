3f929caa6284fca3f5a991ef223b547ea4432e93
Barak Raveh <barak.raveh@gmail.com>
2013-08-15 11:12:01 -0700

    fix default inflate

--- pyext/src/_configuration.py
+++ pyext/src/_configuration.py
@@ -46,7 +46,8 @@ def set_default_configuration(config):
     config.statistics_interval_ns=.001
     config.excluded_volume_k.lower=20
     config.statistics_fraction.lower=.5;
-    config.fg_anchor_inflate_factor=2;
+    config.fg_anchor_inflate_factor=1.0
+    config.are_floaters_on_one_slab_side=0; # false
 
 def add_fg_type(config, type_name, number_of_beads, number, radius,
                 interactions=1, rest_length_factor=1, d_factor=1,
