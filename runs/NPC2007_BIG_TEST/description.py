820fb2ce69ee249271b2d4472ec4ec7bab53e6bb
Barak Raveh <barak@salilab.org>
2013-08-19 22:33:47 -0700

    minor updates

---
+++ runs/NPC2007_BIG_TEST/description.py
@@ -0,0 +1,12 @@
+binary="fg_simulation"
+extra_arguments=["--short_init_factor=.1"]
+tasks=22500
+#300000
+#tasks=2500
+iterations=20
+description=["""
+20x750ns (=15 microsec) iterations of transport in coarse grained
+NPC2007 model (FG motifs per bead = 3)
+scanning:
+1) lotta stuff
+"""]
