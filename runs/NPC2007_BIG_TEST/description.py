4ec41678e55071e92dcea8b705caa568d2a4986c
Barak Raveh <barak.raveh@gmail.com>
2013-08-20 14:34:07 -0700

    shorter sim and more iters

--- runs/NPC2007_BIG_TEST/description.py
+++ runs/NPC2007_BIG_TEST/description.py
@@ -3,9 +3,9 @@ extra_arguments=["--short_init_factor=.1"]
 tasks=22500
 #300000
 #tasks=2500
-iterations=20
+iterations=40
 description=["""
-20x750ns (=15 microsec) iterations of transport in coarse grained
+40x500ns (=20 microsec) iterations of transport in coarse grained
 NPC2007 model (FG motifs per bead = 3)
 scanning:
 1) lotta stuff
