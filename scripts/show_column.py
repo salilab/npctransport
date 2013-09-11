cdf710f7dc04d34b640f8e7a716db9439b05f4f8
Barak Raveh <barak.raveh@gmail.com>
2013-09-11 14:15:43 -0700

    add star option to show column script

--- scripts/show_column.py
+++ scripts/show_column.py
@@ -3,24 +3,38 @@ import re
 import sys
 
 def usage_and_exit():
-    print "\t", sys.argv[0], "<fname> <column_name1> [column_name2] ..."
+    print "\t", sys.argv[0], "<fname> <column_name1> [column_name2] ['*']..."
+    print "'*' - means all other columns, and can be positioned anywhere after fname"
     exit()
 
 def get_column_id(header, names):
-    """ returns the column ids of each name in names, within header """
-#    print names
+    """ returns the column ids of each name in names, within header
+        '*' means all columns that do not appear anywhere else.
+        Only first wildcard is replaced, it is not expected to have more
+        """
+
+    # find name indexes (or mark wildcard '*')
     cols = header.split()
     retval=[]
+    ids_unfound=set( range(len(cols)) ) # in case of wildcard
     for name in names:
-#        print name,
+        if name=='*':
+            retval.append('*')
+            continue
         is_found = False
         for id,col in enumerate(cols):
             if(col == name):
                 retval.append(id)
+                ids_unfound.remove(id)
                 is_found = True
         if(not is_found):
             raise KeyError("column " + name + " not found")
-#    print
+
+    # replace first wildcard with unfound ids, if exists
+    for i,id  in enumerate(retval):
+        if(id=='*'):
+            return retval[0:i] + [x for x in ids_unfound] + retval[i+1:]
+
     return retval
 
 def get_column_id_dict(header, names):
