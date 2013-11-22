e40812d7d1abb8be32a1875be3fc3cde323dd90f
Daniel Russel <drussel@gmail.com>
2013-11-22 14:06:19 -0800

    fix compilation problems with updated RMF

--- src/Statistics.cpp
+++ src/Statistics.cpp
@@ -556,11 +556,11 @@ void Statistics::update
 
   // save RMF for future restarts
   if(!no_save_rmf_to_output){
-    std::string buf;
+    RMF::BufferHandle buf;
     RMF::FileHandle fh = RMF::create_rmf_buffer(buf);
     const_cast<SimulationData *>( get_sd() )->link_rmf_file_handle(fh, false);
     rmf::save_frame(fh, 0);
-    output.set_rmf_conformation(buf);
+    output.set_rmf_conformation(buf.get_string());
   }
 
   // dump to file
