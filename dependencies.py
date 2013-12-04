38bf54dd9b732a9b9f627456cdf7c98aecda7978
Daniel Russel <drussel@gmail.com>
2013-12-04 10:25:54 -0800

    add rmf as a dep

--- dependencies.py
+++ dependencies.py
@@ -1,4 +1,4 @@
-required_modules='container:display:core:atom:statistics:rmf:benchmark:score_functor:kernel:algebra:base'
-lib_only_optional_modules='cgal'
-required_dependencies='RMF:Protobuf:AvroCpp'
-optional_dependencies=""
+required_modules = 'container:display:core:atom:statistics:rmf:benchmark:score_functor:kernel:algebra:base:rmf'
+lib_only_optional_modules = 'cgal'
+required_dependencies = 'RMF:Protobuf:AvroCpp'
+optional_dependencies = ""
