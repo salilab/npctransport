6d5fd8c09a42ec6d7d209faae3f33a4126d2b2d1
Daniel Russel <drussel@gmail.com>
2014-01-14 15:06:12 -0800

    fix another case prob

--- dependency/AvroCpp.cmake
+++ dependency/AvroCpp.cmake
@@ -1,4 +1,4 @@
 set(AVROCPP_INCLUDE_PATH ${CMAKE_SOURCE_DIR}/modules/rmf/dependency/RMF_source/src/avrocpp/api CACHE INTERNAL "" FORCE)
 set(AVROCPP_LIBRARIES "" CACHE INTERNAL "" FORCE)
-list(APPEND IMP_NPCTRANSPORT_LIBRARY_EXTRA_SOURCES avrocpp)
+list(APPEND IMP_npctransport_LIBRARY_EXTRA_SOURCES avrocpp)
 file(WRITE "${CMAKE_BINARY_DIR}/data/build_info/AvroCpp" "ok=True\nincludepath=\"${ACROCPP_INCLUDE_PATH}\"\nlibpath=\"${AVROCPP_LIBRARIES}\"\n")
