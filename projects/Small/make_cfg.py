5dda0c33b277d91a829e7783056a48deaa325760
Barak Raveh <barak@salilab.org>
2014-06-09 14:30:30 -0700

    detailed resolution for short run + add commit_and_run script

--- projects/Small/make_cfg.py
+++ projects/Small/make_cfg.py
@@ -38,7 +38,7 @@ def get_basic_config():
     config.interaction_k.lower=10
     config.interaction_range.lower=1
     config.backbone_k.lower=2.5
-    config.time_step_factor.lower=0.1 #### NOTE THIS ####
+    config.time_step_factor.lower=1 #### NOTE THIS ####
     #create_range(config.rest_length_factor, .5, 1, 10)
     config.time_step_wave_factor.lower=1 #### NOTE THIS ####
     config.excluded_volume_k.lower=0.1
@@ -46,14 +46,14 @@ def get_basic_config():
     config.nonspecific_k.lower=0.025
     config.slack.lower = 7.5
     config.number_of_trials=1
-    config.dump_interval_ns=50
-    config.simulation_time_ns=25000
+    config.dump_interval_ns=0.001
+    config.simulation_time_ns=10
     config.angular_D_factor.lower=1.0 #lower to account for increased dynamic viscosity
                                       # in crowded environment and for coarse graining
     config.statistics_interval_ns=0.1
     config.fg_anchor_inflate_factor=1.0/math.sqrt(fg_coarse_factor)
-    config.is_exclude_floaters_from_slab_initially=1
-    config.are_floaters_on_one_slab_side = 1 # all on top side
+    config.is_exclude_floaters_from_slab_initially=0
+    config.are_floaters_on_one_slab_side = 0 # all on top side
     return config
 
 
@@ -219,7 +219,7 @@ config= get_basic_config()
 # Add floaters
 n_kap_interactions=12
 kaps= IMP.npctransport.add_float_type(config,
-                                     number=100,
+                                     number=30,
                                      radius=kaps_R,
                                       interactions= n_kap_interactions,
                                       type_name="kap")
@@ -230,7 +230,7 @@ create_range(kaps.interaction_k_factor, lb=0.1, ub=5, steps = 5, base=2)
 ##########################################
 #create_range(kaps.radius, lb = 10, ub = 30, steps = 5, base = 1)
 nonspecifics1= IMP.npctransport.add_float_type(config,
-                                              number=100,
+                                              number=30,
                                               radius=kaps_R, #-1,
                                               interactions=0,
                                               type_name="crap0")
@@ -254,7 +254,7 @@ mean_loc=(add_fg_based_on(config, "MRCs/Nup57_16copies_chimera.mrc", k=4, nfgs=1
 
 # add bounding volumes
 config.box_is_on.lower=1
-config.box_side.lower=max(max_z,max_x,max_y)*3.5 # 2000
+config.box_side.lower=max(max_z,max_x,max_y)*2 # 2000
 config.slab_is_on.lower=1
 config.tunnel_radius.lower=max_r - config.fgs[0].radius.lower # or also upper when there's steps?
 config.slab_thickness.lower=250.0 # yeast nuclear envelope - see http://books.google.com/books?id=GvxdK1mdqQwC&pg=PA278&lpg=PA278&dq=yeast+nuclear+envelope+dimensions+nanometer&source=bl&ots=tHQoLfXHI1&sig=nRgZmLYnKuiRNP8n6vhm3bapjpI&hl=en&sa=X&ei=VtwKUtvAAsTAyAHOmIDYBg&ved=0CHsQ6AEwCA#v=onepage&q=yeast%20nuclear%20envelope%20dimensions%20nanometer&f=false
