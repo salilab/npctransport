1108ca5825e18bced214a6f091e382a3e48b52ca
Barak Raveh <barak@salilab.org>
2014-06-10 11:29:36 -0700

    narrower NPC with reduced radius and Nsp1 only for now

---
+++ projects/SmallNPC/run.sh
@@ -0,0 +1,10 @@
+#!/bin/tcsh
+source ~/.aliases
+if ( `git status ./make_cfg.py run2.sh -s | wc -w` > 0 ) then
+    echo commit make_cfg.py and run.sh please
+    exit -1
+endif
+set k=`git log --pretty=format:"%h" -n 1`
+echo GIT revision $k;
+imppy make_cfg.py test_$k.cfg;
+npc_fg_simulation test_$k.cfg --conformations movie_$k.rmf --short_init_factor 0.1 > & LOG_$k &
