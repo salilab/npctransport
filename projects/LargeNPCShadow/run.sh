4281bb1be9ed143c87ba9f00ae974114e2a2194b
Barak Raveh <barak@salilab.org>
2014-06-23 12:16:31 -0700

    minors

--- projects/LargeNPCShadow/run.sh
+++ projects/LargeNPCShadow/run.sh
@@ -6,7 +6,7 @@ if ( `git status ./make_cfg.py run2.sh -s | wc -w` > 0 ) then
 endif
 set R=`git log --pretty=format:"%h" -n 1`
 echo GIT revision $R;
-set k=${R}x$1
+set k=${R}$1
 imppy make_cfg.py test_$k.cfg;
 set init_only="--short_sim_factor 0.00001"
-npc_fg_simulation test_$k.cfg --conformations movie_$k.rmf --short_init_factor 0.1 $init_only  --output output_$k.rmf --statistics_level NONE > & LOG_$k &
+npc_fg_simulation test_$k.cfg --conformations movie_$k.rmf --short_init_factor 0.1 $init_only  --output output_$k.pb --statistics_level NONE > & LOG_$k &
