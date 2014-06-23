20d0f45054104f38b75934530f3bc0c231f7df04
Barak Raveh <barak@salilab.org>
2014-06-23 10:03:00 -0700

    optimized slack and statistics interval params - should not affect simulation results

--- projects/SmallNPC/run.sh
+++ projects/SmallNPC/run.sh
@@ -4,7 +4,9 @@ if ( `git status ./make_cfg.py run2.sh -s | wc -w` > 0 ) then
     echo commit make_cfg.py and run.sh please
     exit -1
 endif
-set k=`git log --pretty=format:"%h" -n 1`
-echo GIT revision $k;
+set R=`git log --pretty=format:"%h" -n 1`
+echo GIT revision $R;
+set k=${R}$1
 imppy make_cfg.py test_$k.cfg;
-npc_fg_simulation test_$k.cfg --conformations movie_$k.rmf --short_init_factor 0.1 --output output_$k.rmf > & LOG_$k &
+set init_only="--short_sim_factor 0.00001"
+npc_fg_simulation test_$k.cfg --conformations movie_$k.rmf --short_init_factor 0.1 $init_only  --output output_$k.pb --statistics_level NONE > & LOG_$k &
