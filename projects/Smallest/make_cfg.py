81b552efa2a27eacce12b9b356d42f6a2349f36a
Barak Raveh <barak@salilab.org>
2014-06-23 11:20:51 -0700

    finer sampling

--- projects/Smallest/make_cfg.py
+++ projects/Smallest/make_cfg.py
@@ -47,8 +47,8 @@ def get_basic_config():
     config.nonspecific_k.lower=0.025
     config.slack.lower = 15
     config.number_of_trials=1
-    config.dump_interval_ns=50
-    config.simulation_time_ns=25000
+    config.dump_interval_ns=0.1
+    config.simulation_time_ns=500
     config.angular_D_factor.lower=1.0 #lower to account for increased dynamic viscosity
                                       # in crowded environment and for coarse graining
     config.statistics_interval_ns=1
