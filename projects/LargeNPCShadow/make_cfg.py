bd071df710d8c7488487c6885df97842fdd32af9
Barak Raveh <barak@salilab.org>
2014-06-26 14:24:27 -0700

    minor fix

--- projects/LargeNPCShadow/make_cfg.py
+++ projects/LargeNPCShadow/make_cfg.py
@@ -173,11 +173,11 @@ def add_fg_based_on(config, mrc_filename, k, nfgs, nres, origin=None,
         max_y = max(max_y, abs(pos.y))
         max_z = max(max_z, abs(pos.z))
     fgs.is_tamd = True;
-    fgs.tamd_T_factor_coeff=1.1
-    fgs.tamd_T_factor_base=1.0
-    fgs.tamd_F_factor_coeff=1.1
-    fgs.tamd_F_factor_base=1.0
-    fgs.tamd_K=1.0
+    fgs.tamd_T_factor_coeff.lower=1.1
+    fgs.tamd_T_factor_base.lower=1.0
+    fgs.tamd_F_factor_coeff.lower=1.1
+    fgs.tamd_F_factor_base.lower=1.0
+    fgs.tamd_K.lower=1.0
     return mean_loc
 
 def add_obstacle(config, mrc_filename, k, R, origin=None):
