1bab52363901cf5d8aeccb1b7697820587c4de0d
Barak Raveh <barak@salilab.org>
2014-07-08 17:14:51 -0700

    show some more output info

--- projects/LargeNPCShadow/show_output.py
+++ projects/LargeNPCShadow/show_output.py
@@ -2,7 +2,17 @@
 from IMP.npctransport import *
 import sys
 f=open(sys.argv[1], "rb")
-config= Output()
-config.ParseFromString(f.read())
-print config.statistics.floaters[1]
-print config.statistics.floaters[2]
+output= Output()
+output.ParseFromString(f.read())
+assign = output.assignment
+print "k", assign.interactions[0].interaction_k
+fg = assign.fgs[0]
+print "tamd params", fg.is_tamd, \
+    fg.tamd_T_factor_coeff.value, \
+    fg.tamd_T_factor_base.value, \
+    fg.tamd_T_factor_coeff.value, \
+    fg.tamd_T_factor_base.value, \
+    fg.tamd_K.value
+print output.statistics.floaters[0]
+print output.statistics.floaters[1]
+print output.statistics.floaters[2]
