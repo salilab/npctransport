f7c66f1b3d1c4beac1d0b816560cdf5f99f66a29
Barak Raveh <barak@salilab.org>
2014-08-26 13:20:08 -0700

    increase fg-kap attraction

--- projects/LargeNPCShadowNI/make_cfg.py
+++ projects/LargeNPCShadowNI/make_cfg.py
@@ -15,7 +15,7 @@ FG_RADIUS_RAW = 11.85 # based on 30A Rg for 125 (even though Rg < surface R)
 
 
 #range_fgfg=FG_RADIUS_RAW*2
-k_fgkap=0.0023
+k_fgkap=0.0046
 k=k_fgkap
 k_skew=0.25 #1.0 #0.25
 rangeN = 10 #14.14 # 10
