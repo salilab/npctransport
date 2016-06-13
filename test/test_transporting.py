453af2e42ec2b61e3f98d05c77872b9a4b946f94
Barak Raveh <barak@salilab.org>
2016-06-13 10:00:12 -0700

    test transporting class

---
+++ test/test_transporting.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+import IMP
+import IMP.test
+import IMP.npctransport
+import IMP.container
+import math
+from test_util import *
+
+radius=20
+
+class Tests(IMP.test.TestCase):
+    def test_transporting(self):
+        m=IMP.Model()
+        p=create_diffusing_rb_particle(m,radius)
+        t=IMP.npctransport.Transporting.setup_particle(p)
+        t.set_is_last_entry_from_top(True);
+        self.assert_(t.get_is_last_entry_from_top())
+        t.set_is_last_entry_from_top(False);
+        self.assert_(not t.get_is_last_entry_from_top())
+        t.set_last_tracked_z(50.0);
+        self.assertEqual(t.get_last_tracked_z(),50.0)
+        t.set_n_entries_bottom(10)
+        self.assertEqual(t.get_n_entries_bottom(),10)
+        t.set_n_entries_top(20)
+        self.assertEqual(t.get_n_entries_top(),20)
+
+if __name__ == '__main__':
+    IMP.test.main()
+
+
+
