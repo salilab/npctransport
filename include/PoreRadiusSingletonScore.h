360516836d3fa846d4d14593bac980f256330f44
Barak Raveh <barak@salilab.org>
2016-12-19 02:06:48 -0800

    initial file for pore radius singleton score

---
+++ include/PoreRadiusSingletonScore.h
@@ -0,0 +1,23 @@
+/**
+ *  \file IMP/npctransport/PoreRadiusSinglestonScore.h
+    \brief - a score on the pore radius equilibrium value
+ *
+ *  Copyright 2007-2016 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPNPCTRANSPORT_PORE_RADIUS_SINGLETON_SCORE_H
+#define IMPNPCTRANSPORT_PORE_RADIUS_SINGLETON_SCORE_H
+
+#include <IMP/npctransport_config.h>
+#include <IMP/core/AttributeSingletonScore.h>
+
+IMPNPCTRANSPORT_BEGIN_NAMESPACE
+
+typedef
+IMP::core::AttributeSingletonScore<harmonic unary function relative to ideal pore>
+PoreRadiusSinglestonScore;
+
+IMPNPCTRANSPORT_END_NAMESPACE
+
+#endif /* IMPNPCTRANSPORT_PORE_RADIUS_SINGLETON_SCORE_H */
