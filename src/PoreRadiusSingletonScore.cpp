d14d529402443a32732e19a59fe76af30154b5c0
Barak Raveh <barak@salilab.org>
2017-01-17 20:32:07 -0800

    add missing file

---
+++ src/PoreRadiusSingletonScore.cpp
@@ -0,0 +1,18 @@
+/**
+ *  \file PoreRadiusSingletonScore.cpp
+ *  \brief XXXX.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/npctransport/PoreRadiusSingletonScore.h"
+
+IMPNPCTRANSPORT_BEGIN_NAMESPACE
+
+ModelObjectsTemp PoreRadiusSingletonScore::do_get_inputs(
+    Model *m, const ParticleIndexes &pis) const {
+  return IMP::get_particles(m, pis);
+}
+
+IMPNPCTRANSPORT_END_NAMESPACE
