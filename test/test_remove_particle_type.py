0bc4057519de2acd0687e95ed9a59aeb76fcec41
Barak Raveh <barak@salilab.org>
2018-06-22 14:37:32 -0700

    add test for remove_fgs_with_prefix

--- test/test_remove_particle_type.py
+++ test/test_remove_particle_type.py
@@ -16,18 +16,19 @@ def get_95_conf(rate,time):
 
 class Tests(IMP.test.TestCase):
 
-    def _make_sd(self, is_orientational=False):
+    def _make_sd(self, is_orientational=False, extra_fg_types=['my_fg2']):
         cfg_file = self.get_tmp_file_name("barak_config.pb")
         assign_file = self.get_tmp_file_name("barak_assign.pb")
         cfg=test_util.make_simple_cfg(is_slab_on=False, n_particles_factor=1)
-        fg2= IMP.npctransport.add_fg_type(cfg,
-                                          type_name="my_fg2",
-                                          number_of_beads=2,
-                                          number=1,
-                                          radius=6,
-                                          interactions=1,
-                                          rest_length_factor = 1.5)
-        cfg.box_side.lower=35
+        for extra_fg_type in extra_fg_types:
+            fg2= IMP.npctransport.add_fg_type(cfg,
+                                              type_name=extra_fg_type,
+                                              number_of_beads=2,
+                                              number=1,
+                                              radius=6,
+                                              interactions=1,
+                                              rest_length_factor = 1.5)
+        cfg.box_side.lower=350
         cfg.time_step_factor.lower=8
         cfg.statistics_interval_ns=0.01
         test_util.write_config_file(cfg_file, cfg)
@@ -62,10 +63,10 @@ class Tests(IMP.test.TestCase):
         return ret
 
 
-    def test_statistics_activation(self):
+    def test_remove_particle_type(self):
         pt1=IMP.core.ParticleType("my_fg")
         pt2=IMP.core.ParticleType("my_fg2")
-        sd= self._make_sd(False)
+        sd= self._make_sd(False, ["my_fg2"])
         timer = IMP.npctransport.create_boost_timer()
         sd.activate_statistics()
 #        sd.set_rmf_file("1.rmf")
@@ -77,6 +78,30 @@ class Tests(IMP.test.TestCase):
         self._run_sd(sd, 50)
         self.assertEqual(len(self._get_particles_of_type(sd, pt1)), 2)
         self.assertEqual(len(self._get_particles_of_type(sd, pt2)), 0)
+
+    def test_remove_particle_typ(self):
+        extra_fg_types_strings=["Nup1", "Nup1b", "Nup1c", "Nup159", "Nup2"]
+        fg_types= [IMP.core.ParticleType(s_fg_type) for s_fg_type in extra_fg_types_strings]
+        fg_types.append(IMP.core.ParticleType("my_fg"))
+        sd= self._make_sd(False, extra_fg_types_strings)
+        timer = IMP.npctransport.create_boost_timer()
+        sd.activate_statistics()
+#        sd.set_rmf_file("1.rmf")
+        self._run_sd(sd, 50)
+        for fg_type in fg_types:
+            self.assertEqual(len(self._get_particles_of_type(sd, fg_type)), 2)
+        sd.remove_fgs_with_prefix("Nup1")
+#        sd.set_rmf_file("2.rmf")
+        self._run_sd(sd, 50)
+        correct_removed_indexes=[0,1,2]
+        correct_kept_indexes=[3,4,5]
+        for i in correct_removed_indexes:
+            self.assertEqual(len(self._get_particles_of_type(sd, fg_types[i])),
+                             0)
+        for i in correct_kept_indexes:
+            self.assertEqual(len(self._get_particles_of_type(sd, fg_types[i])),
+                             2)
+
 
 if __name__ == '__main__':
     IMP.test.main()
