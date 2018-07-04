c508424820f4884949daa7dcf7395deb71351e28
Ben Webb <ben@salilab.org>
2018-07-03 17:39:09 -0700

    Add a Python script to patch protoc output
    The cmake script to patch the protobuf header
    doesn't work on a Windows system since it uses
    symlinks, and Unix tools such as echo and cat.
    Replace it with a Python script which should
    work everywhere.

--- Build.cmake
+++ Build.cmake
@@ -1,11 +1,5 @@
 message(STATUS "Setting up proto " "${CMAKE_BINARY_DIR}/src/npctransport/npctransport.pb.cpp")
 
-if(WIN32)
-  set(COPY_OR_LINK "copy")
-else(WIN32)
-  set(COPY_OR_LINK "create_symlink")
-endif(WIN32)
-
 # there is a #include 'npctransport.ph.h' in the cpp file
 add_custom_command(OUTPUT "${CMAKE_BINARY_DIR}/include/IMP/npctransport/internal/npctransport.pb.h"
                           "${CMAKE_BINARY_DIR}/src/npctransport/npctransport.pb.h"
@@ -13,13 +7,10 @@ add_custom_command(OUTPUT "${CMAKE_BINARY_DIR}/include/IMP/npctransport/internal
                           COMMAND protoc "--cpp_out=dllexport_decl=IMPNPCTRANSPORTEXPORT:."
                           "-I${CMAKE_SOURCE_DIR}/modules/npctransport/data/"
                           "${CMAKE_SOURCE_DIR}/modules/npctransport/data/npctransport.proto"
-                          COMMAND mv npctransport.pb.cc npctransport.pb.cpp
-                          # add config header to resolve export symbols
-                          COMMAND mv "${CMAKE_BINARY_DIR}/src/npctransport/npctransport.pb.h" "${CMAKE_BINARY_DIR}/src/npctransport/npctransport.pb.h.out"
-                          COMMAND echo "\"\#include\"" "\"<IMP/npctransport/npctransport_config.h>\"" > "${CMAKE_BINARY_DIR}/src/npctransport/npctransport.pb.h"
-                          COMMAND cat "${CMAKE_BINARY_DIR}/src/npctransport/npctransport.pb.h.out" >> "${CMAKE_BINARY_DIR}/src/npctransport/npctransport.pb.h"
-                          COMMAND rm -f "${CMAKE_BINARY_DIR}/include/IMP/npctransport/internal/npctransport.pb.h"
-			  COMMAND ${CMAKE_COMMAND} -E ${COPY_OR_LINK} "${CMAKE_BINARY_DIR}/src/npctransport/npctransport.pb.h" "${CMAKE_BINARY_DIR}/include/IMP/npctransport/internal/npctransport.pb.h"
+			  COMMAND ${CMAKE_COMMAND} -E rename npctransport.pb.cc npctransport.pb.cpp
+			  COMMAND python "${CMAKE_SOURCE_DIR}/modules/npctransport/patch_protoc.py"
+			                 "${CMAKE_BINARY_DIR}/src/npctransport/npctransport.pb.h"
+					 "${CMAKE_BINARY_DIR}/include/IMP/npctransport/internal/npctransport.pb.h"
                           DEPENDS "${CMAKE_SOURCE_DIR}/modules/npctransport/data/npctransport.proto"
                           COMMENT "Creating protoc stuff for npctransport"
                           WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/src/npctransport")
