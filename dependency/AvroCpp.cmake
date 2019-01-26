5a3e6dc96354c924c083fcec83068205d7c5018e
Ben Webb <ben@salilab.org>
2019-01-26 12:13:16 -0800

    Allow overriding AVROCPP_INCLUDE_PATH
    If we build npctransport outside of IMP,
    the default path to the Avro headers will
    be incorrect. Allow overriding it from
    the cmake command line so we can point it
    to a copy of RMF.

--- dependency/AvroCpp.cmake
+++ dependency/AvroCpp.cmake
@@ -1,4 +1,6 @@
-set(AVROCPP_INCLUDE_PATH ${CMAKE_SOURCE_DIR}/modules/rmf/dependency/RMF/src/avrocpp/api CACHE INTERNAL "" FORCE)
+if(NOT DEFINED AVROCPP_INCLUDE_PATH)
+  set(AVROCPP_INCLUDE_PATH ${CMAKE_SOURCE_DIR}/modules/rmf/dependency/RMF/src/avrocpp/api CACHE INTERNAL "" FORCE)
+endif()
 set(AVROCPP_LIBRARIES "" CACHE INTERNAL "" FORCE)
 list(APPEND IMP_npctransport_LIBRARY_EXTRA_SOURCES avrocpp)
 file(WRITE "${CMAKE_BINARY_DIR}/build_info/AvroCpp" "ok=True\nincludepath=\"${ACROCPP_INCLUDE_PATH}\"\nlibpath=\"${AVROCPP_LIBRARIES}\"\n")
