496b1f68f69ec73090d3dea000f8ad9110719b7c
Ben Webb <ben@salilab.org>
2021-06-04 13:27:12 -0700

    We don't need nose any more (we use pytest)

--- tools/setup_ci.sh
+++ tools/setup_ci.sh
@@ -10,7 +10,7 @@ fi
 python_version=$1
 
 conda config --remove channels defaults  # get conda-forge, not main, packages
-conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} numpy pip nose imp-nightly gxx_linux-64 eigen swig cmake protobuf
+conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} numpy pip imp-nightly gxx_linux-64 eigen swig cmake protobuf
 eval "$(conda shell.bash hook)"
 conda activate python${python_version}
 pip install pytest-cov coverage pytest-flake8
