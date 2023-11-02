2d6f4e2ec103e8cdbf36b2a3d51c7f09f8bc9a0c
barakr <barak.raveh@gmail.com>
2023-11-02 13:32:45 +0200

    add test for precise densities in HDF5

--- test/expensive_test_hdf5_statistics.py
+++ test/expensive_test_hdf5_statistics.py
@@ -11,41 +11,65 @@ import numpy as np
 import subprocess
 
 radius=8
-N_OUTPUT_FRAMES = 25
+N_OUTPUT_FRAMES = 15
 N_FULL_OUTPUT_PER_HDF5 = 5
-BOX_SIDE=800
+BOX_SIDE_A=300
+XYZ_STATS_CROP_FACTOR=1.0
+XYZ_STATS_MAX_BOX_SIZE_A=1000
+VOXEL_SIZE_A=10
+N_FG_BEADS=2
+N_FGs=1
+N_KAPs=1
+N_INERTs=1
+N_STATS_PER_HDF5=30.0
 
-def get_95_conf(rate,time):
-    if time==0.0:
-        return -1.0
-    return 1.96*math.sqrt(rate/time)
+def my_make_simple_cfg(outfile, 
+                       fg_name = "fg", 
+                       kap_name = "kap", 
+                       inert_name="inert",
+                       is_multiple_hdf5s=False):
+    cfg= test_util.get_basic_config()
+    IMP.npctransport.add_fg_type(cfg,
+                                 type_name=fg_name,
+                                 number_of_beads=N_FG_BEADS,
+                                 number=N_FGs,
+                                 radius=8,
+                                 interactions=1,
+                                 rest_length_factor = 1.5)
+    IMP.npctransport.add_float_type(cfg,
+                                    type_name=kap_name,
+                                    number=N_KAPs,
+                                    radius=20,
+                                    interactions=6)
+    IMP.npctransport.add_float_type(cfg,
+                                    type_name=inert_name,
+                                    number=N_INERTs,
+                                    radius=20,
+                                    interactions=0)
+    cfg.box_is_on.lower=1
+    cfg.box_side.lower=BOX_SIDE_A
+    cfg.slab_is_on.lower= False
+    cfg.nonspecific_range.lower= 5.0
+    cfg.nonspecific_k.lower= 0.01
+    cfg.time_step_factor.lower=8
+    cfg.is_xyz_hist_stats=True
+    cfg.xyz_stats_crop_factor=XYZ_STATS_CROP_FACTOR
+    cfg.xyz_stats_max_box_size_a=XYZ_STATS_MAX_BOX_SIZE_A
+    cfg.xyz_stats_voxel_size_a=VOXEL_SIZE_A
+    cfg.is_multiple_hdf5s = is_multiple_hdf5s
+    cfg.dump_interval_ns=1.0
+    cfg.output_statistics_interval_ns = 1.0
+    cfg.statistics_interval_ns = cfg.output_statistics_interval_ns / N_STATS_PER_HDF5
+    cfg.simulation_time_ns = N_OUTPUT_FRAMES * cfg.output_statistics_interval_ns
+    cfg.full_output_statistics_interval_factor = N_FULL_OUTPUT_PER_HDF5
+    cfg.statistics_fraction.lower=1.0
+    if outfile is not None:
+        test_util.write_config_file(outfile, cfg)
+    return cfg
 
 class Tests(IMP.test.TestCase):
-    
-    def _make_cfg(self, cfg_file, is_orientational, is_multiple_hdf5s):
-        cfg=test_util.make_simple_cfg(is_slab_on=False, n_particles_factor=1, is_obstacles = 1,
-                                      fg_name = "fg", kap_name = "kap", inert_name="inert")
-        cfg.box_side.lower=BOX_SIDE
-        cfg.time_step_factor.lower=8
-        cfg.statistics_interval_ns=0.01
-        for i in cfg.interactions:
-            if i.type0=="kap0" or i.type1=="kap":
-                i.interaction_range.lower=10
-                if(is_orientational):
-                    i.range_sigma0_deg.lower=45
-                    i.range_sigma1_deg.lower=45
-                    i.interaction_k.lower=0.18
-                else:
-                    i.interaction_k.lower=0.45
-        cfg.is_xyz_hist_stats = True
-        cfg.is_multiple_hdf5s = is_multiple_hdf5s
-        cfg.output_statistics_interval_ns = 1.0
-        cfg.simulation_time_ns = N_OUTPUT_FRAMES * cfg.output_statistics_interval_ns
-        cfg.full_output_statistics_interval_factor = N_FULL_OUTPUT_PER_HDF5
-        cfg.statistics_fraction.lower=1.0
-        test_util.write_config_file(cfg_file, cfg)
 
-    def _validate_hdf5_file(self, hdf5_filename):
+    def _validate_hdf5_file(self, hdf5_filename, is_multiple_hdf5s):
         print("Handling {}".format(hdf5_filename))
         F = RMF.HDF5.open_file(hdf5_filename)
         G_fgs= F.get_child_group("fg_xyz_hist")
@@ -55,10 +79,17 @@ class Tests(IMP.test.TestCase):
             fg_type= G_fgs.get_child_name(j)
             self.assertEqual(fg_type, "fg")
             dataset= G_fgs.get_child_int_data_set_3d(fg_type)
-            n_voxels_expected = min(500, BOX_SIDE/2)/10 + 5*2 # adding 5 voxels on each side used for slack
+            n_voxels_expected = XYZ_STATS_CROP_FACTOR * min(XYZ_STATS_MAX_BOX_SIZE_A, BOX_SIDE_A) / VOXEL_SIZE_A \
+                                    + 5*2 # adding 5 voxels on each side used for slack
             self.assertEqual(dataset.get_size(), 
                              [n_voxels_expected, n_voxels_expected, n_voxels_expected])
             XYZ_as_vector=np.array(dataset.get_block([0,0,0],dataset.get_size()))
+            self.assertEqual(XYZ_as_vector.shape, (n_voxels_expected**3,))
+            print("Sum of XYZ: {}".format(np.sum(XYZ_as_vector)))
+            sum_observed = np.sum(XYZ_as_vector)
+            sum_expected = ( N_FG_BEADS * N_FGs * N_STATS_PER_HDF5  
+                              * (1 if is_multiple_hdf5s else N_OUTPUT_FRAMES))
+            self.assertAlmostEqual(sum_observed, sum_expected, delta=0.1*sum_observed)
         G_floaters= F.get_child_group("floater_xyz_hist")
         n_floaters= G_floaters.get_number_of_children()
         self.assertEqual(n_floaters, 2)
@@ -67,6 +98,13 @@ class Tests(IMP.test.TestCase):
             print("Floater type: {}".format(floater_type))
             dataset= G_floaters.get_child_int_data_set_3d(floater_type)
             XYZ_as_vector=np.array(dataset.get_block([0,0,0],dataset.get_size()))
+            self.assertEqual(XYZ_as_vector.shape, (n_voxels_expected**3,))
+            print("Sum of XYZ: {}".format(np.sum(XYZ_as_vector)))
+            sum_observed = np.sum(XYZ_as_vector)
+            sum_expected = ( (N_KAPs if floater_type=="kap" else N_INERTs) * N_STATS_PER_HDF5  
+                              * (1 if is_multiple_hdf5s else N_OUTPUT_FRAMES))
+            self.assertAlmostEqual(sum_observed, sum_expected, delta=0.1*sum_observed)
+            
                          
     def _test_xyz_stats_using_main(self, 
                                    short_sim_factor,
@@ -78,11 +116,10 @@ class Tests(IMP.test.TestCase):
             print("*** Multiple HDF5s ***" if is_multiple_hdf5s else "*** Single HDF5 ***")
             cfg_file = "config{}.pb".format(is_multiple_hdf5s)
             out_file = "output{}.pb".format(is_multiple_hdf5s)
-            cfg_file = self.get_tmp_file_name(cfg_file)
-            out_file = self.get_tmp_file_name(out_file)
-            self._make_cfg(cfg_file,
-                           is_orientational=is_orientational, 
-                           is_multiple_hdf5s=is_multiple_hdf5s)
+            #cfg_file = self.get_tmp_file_name(cfg_file)
+            #out_file = self.get_tmp_file_name(out_file)
+            my_make_simple_cfg(cfg_file,
+                                is_multiple_hdf5s=is_multiple_hdf5s)
             # create a temporary python file and call it
             python_file = self.get_tmp_file_name("fake_main.py")
             with open(python_file, 'w') as f:
@@ -103,7 +140,8 @@ IMP.npctransport.do_main_loop(sd, [])
                 self.assertEqual(len(hdf5_filenames), 1)
             print("HDF5 files: {}".format(hdf5_filenames))
             for hdf5_filename in hdf5_filenames:
-                self._validate_hdf5_file(hdf5_filename)
+                self._validate_hdf5_file(hdf5_filename, 
+                                         is_multiple_hdf5s=is_multiple_hdf5s)
                     
 
     def test_hdf5_statistics(self):
