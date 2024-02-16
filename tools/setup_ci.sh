d4c2270dc33ec94a7a188ee3e61461a6ff5b5ab4
Ben Webb <ben@salilab.org>
2024-02-16 11:34:57 -0800

    We need crypt.h for conda, not the OS

--- tools/setup_ci.sh
+++ tools/setup_ci.sh
@@ -12,12 +12,15 @@ python_version=$1
 if [ ${python_version} = "2.7" ]; then
   BOOST=""
   pip="pip<=19.3.1"
+  # Python.h includes crypt.h, which is no longer provided by default
+  crypt="libxcrypt"
 else
   BOOST="libboost-devel"
   pip="pip"
+  crypt=""
 fi
 conda config --remove channels defaults  # get conda-forge, not main, packages
-conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} numpy ${pip} imp-nightly ${BOOST} gxx_linux-64 eigen cereal swig cmake protobuf
+conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} numpy ${pip} ${crypt} imp-nightly ${BOOST} gxx_linux-64 eigen cereal swig cmake protobuf
 eval "$(conda shell.bash hook)"
 conda activate python${python_version}
 
@@ -25,8 +28,6 @@ if [ ${python_version} = "2.7" ]; then
   # pytest-flake8 1.1.0 tries to import contextlib.redirect_stdout, which
   # isn't present in Python 2
   pip install pytest-cov coverage 'pytest-flake8<1.1'
-  # Python.h includes crypt.h, which is no longer provided by default
-  sudo apt-get install -y libcrypt-dev
 else
   pip install pytest-cov coverage pytest-flake8
 fi
